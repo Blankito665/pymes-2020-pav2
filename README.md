ece6fe665c9cf542eb6eafd57b91fefa4905e551
Blankito665 <joaquinbas98@gmail.com>
2020-07-07 19:50:22 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pymes-2020-pav2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/pymes-2020-pav2)
---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { InicioComponent } from './component/inicio/inicio.component';
+import { ArticulosFamiliasComponent } from './component/articulos-familias/articulos-familias.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, InicioComponent, ArticulosFamiliasComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
