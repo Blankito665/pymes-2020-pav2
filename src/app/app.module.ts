5c53ebb24342e10cd09a63b1c7ece14922646fcf
Blankito665 <joaquinbas98@gmail.com>
2020-07-20 17:28:33 -0300

    2 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from "@angular/common/http";
 import { RouterModule } from '@angular/router';
 import {APP_BASE_HREF} from '@angular/common';  
+import { ReactiveFormsModule } from "@angular/forms";
 
 
 import { AppComponent } from './app.component';
@@ -13,16 +14,19 @@ import { ArticulosFamiliasComponent } from './component/articulos-familias/artic
 import { MockArticulosFamiliasService } from './services/mock-articulos-familias.service';
 import { ArticulosFamiliasService } from './services/articulos-familias.service';
 import { MenuComponent } from './component/menu/menu.component';
+import { ArticulosComponent } from './component/articulos/articulos.component';
+import { MockArticulosService } from './services/mock-articulos.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule,  RouterModule.forRoot([
+  imports:      [ BrowserModule, FormsModule, HttpClientModule,ReactiveFormsModule,  RouterModule.forRoot([
       { path: '', redirectTo: '/inicio', pathMatch: 'full' },
       { path: 'inicio', component: InicioComponent },
-      { path: 'articulosfamilias', component: ArticulosFamiliasComponent }
+      { path: 'articulosfamilias', component: ArticulosFamiliasComponent },
+      { path: 'articulos', component: ArticulosComponent }
     ])
  ],
-  declarations: [ AppComponent, HelloComponent, InicioComponent, ArticulosFamiliasComponent, MenuComponent ],
+  declarations: [ AppComponent, HelloComponent, InicioComponent, ArticulosFamiliasComponent, MenuComponent, ArticulosComponent ],
   bootstrap:    [ AppComponent ],
-  providers: [MockArticulosFamiliasService, ArticulosFamiliasService]
+  providers: [MockArticulosFamiliasService, ArticulosFamiliasService, MockArticulosService]
 })
 export class AppModule { }
